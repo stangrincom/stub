5029ea2b5a3c0b5e598c3ecfc9a32f6ac05488c5
stan <in@stangrin.com>
2020-02-03 02:48:41 +0300

    ss

---
+++ docs/precache-manifest.1111771efd546d947019516bec7e0ef1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a1fb6bdc621597a6b8abe720384028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bee60953fd60a75253d2",
+    "url": "/static/css/main.245551ae.chunk.css"
+  },
+  {
+    "revision": "295171a2b996f98d250e",
+    "url": "/static/js/2.19f60bd9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.19f60bd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee60953fd60a75253d2",
+    "url": "/static/js/main.1be96545.chunk.js"
+  },
+  {
+    "revision": "6c07a24d129f9ae8fc6e",
+    "url": "/static/js/runtime-main.02c9e19b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
