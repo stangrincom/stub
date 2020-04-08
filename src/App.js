d0c0b52cf9a050c4a19b28e194fc09f62f7df9aa
stan <in@stangrin.com>
2020-04-08 18:22:34 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './1.png';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        Проводятся технические работы
-        </p>
-        <p>
-          Зайдите через 1 час
-        </p>
-      </header>
+    <div className="app">
+      <div className="app__center">
+        <div className="app__icon">
+           <img className={"app__img"} src={logo} />
+        </div>
+        <div className="app__title">
+          Уважаемые участники!
+        </div>
+        <div className="app__text">
+             9 апреля каталог Вектор будет закрыт на технические работы.
+              Каталог будет доступен с 10.04
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// Уважаемые участники!
+// </p>
+// <p className={"helper"}>
+//     9 апреля каталог Вектор будет закрыт на технические работы.
+//     Каталог будет доступен с 10.04
